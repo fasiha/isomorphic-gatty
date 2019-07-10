08ac532ff41cefc535451635df11d99121db56af
Ahmed Fasih <wuzzyview@gmail.com>
2019-07-09 23:10:12 -0400

    atomic appends added, not tested

--- index.ts
+++ index.ts
@@ -17,8 +17,8 @@ export type Gatty = {
   token: string|undefined,
 };
 
-export async function setup(url: string, {dir, corsProxy, branch, depth, since, username, password, token}:
-                                             Partial<Gatty> = {}): Promise<Gatty> {
+export async function setup({dir, corsProxy, branch, depth, since, username, password, token}: Partial<Gatty> = {},
+                            url: string): Promise<Gatty> {
   dir = dir || DEFAULT_DIR;
   corsProxy = corsProxy || DEFAULT_PROXY;
 
@@ -36,25 +36,72 @@ export async function push({dir, username, password, token}: Gatty) {
   return pushres.errors;
 }
 
-export async function ls(filepath: string = '', {dir, pfs}: Gatty): Promise<string[]> {
+export async function ls({dir, pfs}: Gatty, filepath: string = ''): Promise<string[]> {
   return pfs.readdir(filepath ? `${dir}/${filepath}`.replace(/\/+$/, '') : dir);
 }
 
-export async function readFile(filepath: string, {dir, pfs}: Gatty): Promise<string> {
+export async function readFile({dir, pfs}: Gatty, filepath: string): Promise<string> {
   return pfs.readFile(`${dir}/${filepath}`, 'utf8');
 }
 
-export async function writeFileCommit(filepath: string, contents: string, message: string, name: string = 'Me',
-                                      email: string = 'mrtest@example.com', {pfs, dir}: Gatty): Promise<string> {
+export async function writeFileCommit({pfs, dir}: Gatty, filepath: string, contents: string, message: string,
+                                      name: string = 'Me', email: string = 'mrtest@example.com'): Promise<string> {
   await pfs.writeFile(`${dir}/${filepath}`, contents, 'utf8');
   await git.add({dir, filepath});
   return git.commit({dir, message, author: {name, email}});
 }
 
-export async function atomicAppend(filepath: string, content: string, maxRetries = 3, {pfs, dir}: Gatty) {
+type PushResult = {
+  ok: string,
+  errors?: string[],
+};
+export async function atomicAppend({pfs, dir, username, password, token}: Gatty, filepath: string, content: string,
+                                   message: string, name: string, email: string, maxRetries = 3) {
+  for (let retry = 0; retry < maxRetries; retry++) {
+    await git.pull({dir, singleBranch: true, fastForwardOnly: true, username, password, token});
+    const fullpath = `${dir}/${filepath}`;
+    let oldContents = '';
+    try {
+      await pfs.stat(fullpath)
+      oldContents = await pfs.readFile(fullpath, 'utf8');
+    } catch (e) { await pfs.writeFile(fullpath, '', 'utf8'); }
+    await pfs.writeFile(fullpath, oldContents + content, 'utf8');
+    await git.commit({dir, message, author: {name, email}});
+    const pushed: PushResult = await git.push({dir, username, password, token});
+    if (pushed.errors) {
+      const branch = await git.currentBranch({dir});
+      await gitReset({pfs, git, dir, ref: 'HEAD~1', branch, hard: true});
+    }
+  }
   // pull to remote
   // write/append
   // commit
   // push
   // if push failed, roll back commit and retry, up to some maximum
+}
+
+type GitResetArgs = {
+  pfs: any,
+  git: any,
+  dir: string,
+  ref: string,
+  branch: string,
+  hard?: boolean
+};
+// Thanks to jcubic: https://github.com/isomorphic-git/isomorphic-git/issues/729#issuecomment-489523944
+async function gitReset({pfs, git, dir, ref, branch, hard}: GitResetArgs) {
+  hard = hard || false;
+  const re = /^HEAD~([0-9]+)$/;
+  const m = ref.match(re);
+  if (!m) { throw new Error(`Wrong ref ${ref}`) }
+  const count = +m[1];
+  const commits = await git.log({dir, depth: count + 1});
+  if (commits.length < count + 1) { throw new Error('Not enough commits'); }
+  const commit: string = commits[commits.length - 1].oid;
+  await pfs.writeFile(`${dir}/.git/refs/heads/${branch}`, commit + '\n');
+  if (!hard) { return }
+  // clear the index (if any)
+  await pfs.unlink(`${dir}/.git/index`);
+  // checkout the branch into the working tree
+  git.checkout({dir, ref: branch});
 }
